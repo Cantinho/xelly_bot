797895e4b1beb4f8776ca7690865ee5e59db3cbd
Samir Trajano Feitosa <samir.feitosa@virtus.ufcg.edu.br>
2018-01-31 23:52:15 -0300

    initial commit

---
+++ xellybot-spring/xelly/src/main/java/br/com/cantinho/xelly/XellyApplication.java
@@ -0,0 +1,12 @@
+package br.com.cantinho.xelly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XellyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XellyApplication.class, args);
+	}
+}
